b38dd3950325f186ef2af8a47dea3da5a6652b4f
고영광 <a.@ui-MacBookPro.local>
2025-08-17 04:29:02 +0900

    Add custom head with favicon

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>3000.surgery</title>
+      <meta name="description" content="3000.surgery" />
+      <link rel="icon" href="/favicon_32x32.png" />
+    </>
+  );
+}
