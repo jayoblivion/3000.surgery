9e9de2ceec82f83779eb9708e4a52e61031c4c5f
고영광 <a.@ui-MacBookPro.local>
2025-08-17 04:37:15 +0900

    fix: update favicon with cache busting

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,10 @@
+// app/head.tsx
 export default function Head() {
   return (
     <>
       <title>3000.surgery</title>
       <meta name="description" content="3000.surgery" />
-      <link rel="icon" href="/favicon.png" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </>
   );
 }
