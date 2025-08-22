518350f1ac4235e98f37435a6e124378bfeeb2d8
고영광 <a.@ui-MacBookPro.local>
2025-08-23 06:43:54 +0900

    ci(vercel): add tailwind/postcss deps & configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: { extend: {} },
   plugins: [],
